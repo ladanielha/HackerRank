2390a55900fa046015106b2aa56a4e7a8663511c
LaDanielHa <laurentiusdaniel0@gmail.com>
2024-11-01 20:26:29 +0700

    a

---
+++ select_country_jpn_2.sql
@@ -0,0 +1,21 @@
+/*
+Query all columns for every row in the city table 
+The City table described ad follows: 
+
+
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
++-------------+--------------+
+
+ */
+
+select * 
+from city 
+where countrycode = 'JPN'
+
---
+++ select_query_by_id.sql
@@ -0,0 +1,14 @@
+/*
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
++-------------+--------------+
+
+ */
+
+select * from city where id = 1661
