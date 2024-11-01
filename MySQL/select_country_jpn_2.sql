f1342e03839ffc5df3c69893c838fdc1f73bdcdd
LaDanielHa <laurentiusdaniel0@gmail.com>
2024-11-01 20:27:59 +0700

    a

--- MySQL/select_country_jpn_2.sql
+++ MySQL/select_country_jpn_2.sql
@@ -19,10 +19,3 @@ select *
 from city 
 where countrycode = 'JPN'
 
--- Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
--- The CITY table is described as follows:
-
-select name
-from city 
-where countrycode = 'JPN'
-
--- MySQL/select_country_jpn_3.sql
+++ MySQL/select_country_jpn_3.sql
@@ -1,3 +1,7 @@
+
+-- Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
+-- The CITY table is described as follows:
+
 /*
 +-------------+--------------+
 | Field       | Type         |
@@ -11,4 +15,6 @@
 
  */
 
-select * from city where id = 1661
+select name
+from city 
+where countrycode = 'JPN'
